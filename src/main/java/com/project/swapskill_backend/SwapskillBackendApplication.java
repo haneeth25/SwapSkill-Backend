4c263eb10faf351e5c8944ac4371171181748014
haneethkosaraju <captaina857@gmail.com>
2025-05-26 17:54:20 +0530

    Created backend project for swapskill

---
+++ src/main/java/com/project/swapskill_backend/SwapskillBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.swapskill_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapskillBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapskillBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/swapskill_backend/SwapskillBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.swapskill_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapskillBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
